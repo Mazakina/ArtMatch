27b178817aae18a4ecdde2afec9b57ef0d89ea1b
Paulo Mazakina <8.mazakina@gmail.com>
2022-12-01 03:37:06 -0300

    cors_solved

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from  'axios'
 
 export const Api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://art-match-s16i.vercel.app/api',
 })
 
 export function saveImage(data){ Api.post('/_lib/manageGallery',data)}
