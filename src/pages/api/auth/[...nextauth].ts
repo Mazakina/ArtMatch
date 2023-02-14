211da4a412d6a8e0662b07f6dc17f50944b042aa
Paulo Mazakina <8.mazakina@gmail.com>
2023-02-13 21:19:10 -0300

    InkTrail.1.0

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -26,7 +26,6 @@ export const authOptions = {
       }
     }),
     
-    // ...add more providers here
   ],
   secret:'iAmHandsome',
   jwt: {
@@ -154,7 +153,6 @@ export const authOptions = {
       }
     },
     async jwt({ token, account, profile }) {
-      // Persist the OAuth access_token and or the user id to the token right after signin
       if (account) {
         token.accessToken = account.access_token
         token.id = profile.id
@@ -162,7 +160,6 @@ export const authOptions = {
       return token
     },
     async session({ session, token, user }) {
-      // Send properties to the client, like an access_token and user id from a provider.
       session.accessToken = token.accessToken
       session.user.id = token.id
       
