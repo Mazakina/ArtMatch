885bf54b49e88e91925d82621f5057cdbbbc0b06
Paulo Mazakina <8.mazakina@gmail.com>
2023-02-16 02:39:45 -0300

    InkTrail.1.01

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document(){
   return(
     <Html>
       <Head>
+        <meta charSet="UTF-8" />
+        <meta lang='pt-br' />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;400;600&display=swap" rel="stylesheet"/>
